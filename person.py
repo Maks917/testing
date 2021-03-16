14795ebc7113665d2e4ebc9832a1e28bdf19a83f
Maksat <mkosaibek@mail.ru>
2021-03-16 23:23:49 +0600

    second commit

---
+++ person.py
@@ -0,0 +1,7 @@
+class Person:
+
+    def __init__(self, firstname, middlename, nickname, birthYear):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.nickname = nickname
+        self.birthYear = birthYear
